70d44015e14ce2981f217e578b887b4447fd39e5
Bronson-Brown-deVost <bronsonbdevost@aim.com>
2017-06-03 08:42:32 +0200

    Testing.

--- resources/js/single-image-control.js
+++ resources/js/single-image-control.js
@@ -1,5 +1,5 @@
 var infoJsonUrl = 'http://134.76.19.179/cgi-bin/iipsrv.fcgi?IIIF=P1096-Fg006-R-C01-R01-D25112012-T112913-LR445__ColorCalData_IAA_Both_CC110304_110702.ptif/info.json';
-var infoJson;
+//var infoJson;
 jQuery.getJSON(infoJsonUrl).done(function (infoJson, status, jqXHR) {
 	var viewer = OpenSeadragon({
 		id: "single-image-view",
@@ -9,6 +9,7 @@ jQuery.getJSON(infoJsonUrl).done(function (infoJson, status, jqXHR) {
 		defaultZoomLevel: 1,
 		sequenceMode: false,
 		tileSources: infoJson,
+		ajaxWithCredentials : true,
 		prefixUrl: 'resources/images/',
 		navigationControlAnchor: OpenSeadragon.ControlAnchor.BOTTOM_LEFT,
 		crossOriginPolicy: 'Anonymous'
