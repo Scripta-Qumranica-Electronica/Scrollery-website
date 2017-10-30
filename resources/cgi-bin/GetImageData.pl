f8b9703c8368c2c1232da86badd6ea1ef27a1e3d
Bronson-Brown-deVost <bronsonbdevost@aim.com>
2017-10-30 12:55:00 +0100

    Began adding Heredoc support.

--- resources/cgi-bin/GetImageData.pl
+++ resources/cgi-bin/GetImageData.pl
@@ -138,8 +138,21 @@ sub getImagesOfFragment {
 	my $sql;
 	my $idType = $cgi->param('idType');
 	my $id = $cgi->param('id');
+	my $query = <<'MYSQL';
+SELECT 	SQE_image.filename as filename,
+		SQE_image.wavelength_start as start,
+		SQE_image.wavelength_end as end,
+		SQE_image.is_master,
+		image_urls.url as url
+	FROM SQE_image
+		INNER JOIN image_urls on image_urls.id = SQE_image.url_code
+		INNER JOIN image_catalog ON image_catalog.image_catalog_id = SQE_image.image_catalog_id
+		INNER JOIN image_to_edition_catalog on image_to_edition_catalog.catalog_id = image_catalog.image_catalog_id
+	WHERE image_to_edition_catalog.edition_id = ?
+MYSQL
+
 	if ($idType eq 'composition') {
-		$sql = $cgi->dbh->prepare_cached('SELECT SQE_image.filename as filename, SQE_image.wavelength_start as start, SQE_image.wavelength_end as end, SQE_image.is_master, image_urls.url as url FROM SQE_image INNER JOIN image_urls on image_urls.id = SQE_image.url_code INNER JOIN image_catalog ON image_catalog.image_catalog_id = SQE_image.image_catalog_id INNER JOIN image_to_edition_catalog on image_to_edition_catalog.catalog_id = image_catalog.image_catalog_id WHERE image_to_edition_catalog.edition_id = ?') 
+		$sql = $cgi->dbh->prepare_cached($query)
 		or die "Couldn't prepare statement: " . $cgi->dbh->errstr;
 	} elsif ($idType eq 'institution') {
 		$sql = $cgi->dbh->prepare_cached('SELECT * FROM SQE_image WHERE image_catalog_id = ?') 
