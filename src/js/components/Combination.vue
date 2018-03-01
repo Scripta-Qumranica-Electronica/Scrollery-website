2db6799c71dbafab82e18914e5e7921f9807495e
Bronson Brown-deVost <bronsonbdevost@aim.com>
2018-03-01 14:03:40 +0100

    Enhanced Combination.vue zoom slider.

--- src/js/components/Combination.vue
+++ src/js/components/Combination.vue
@@ -1,13 +1,21 @@
 <template>
   <div style="{width: 100%; height: 100%;}">
     <div class="row align-middle" style="{width: 100%; height 50px;}">
-        <label class="col-1">Zoom {{globalScale}}</label>
-        <input  class="col-2" 
+        <label class="col-1">Zoom</label>
+        <el-slider 
+            class="col-1" 
+            v-model="scale"
+            :min="1"
+            :step="0.5"
+            :max="100"
+            :format-tooltip="formatTooltip">
+        </el-slider>
+        <!-- <input  class="col-2" 
                 type="range" 
                 min="0.01" 
                 max="1.0" 
                 step="0.01" 
-                v-model="scale" />
+                v-model="scale" /> -->
     </div>
     <div style="{width: 100%; height: calc(100% - 50px); overflow: auto;}">
         <combination-canvas :global-scale="globalScale"></combination-canvas>
@@ -23,13 +31,20 @@ export default {
   },
   data() {
     return {
-        scale: 0.1,
+        scale: 0.01,
     }
   },
   computed: {
       globalScale() {
-          return Number(this.scale)
+        // This returns a logarithmical scale value for
+        // the current slider position.
+        return parseFloat((((100-1)*Math.pow((this.scale / 100), 3) + 1) / 100).toFixed(4));
       }
   },
+  methods: {
+      formatTooltip(val) {
+          return (this.globalScale * 100).toFixed(2) + '%'
+      }
+  }
 }
 </script>
