1c04c70c718256e4abe1ac39872f08dcb7b8ce4c
sjones6 <jones.spencera@gmail.com>
2018-03-19 20:45:08 -0400

    Migrate dev-server and ajax requests to POST with JSON

--- dev-server.js
+++ dev-server.js
@@ -4,8 +4,8 @@ const middleware = require('webpack-dev-middleware')
 const conf = require('./webpack.server.js')
 const compiler = webpack(conf)
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
-const formidable = require('formidable')
 
 const { exec } = require('child_process')
 
@@ -13,49 +13,47 @@ const perl = (req, res) => {
 
   const sendErr = msg => res.status(500).send(msg || "Request Failed")
   
-  var form = new formidable.IncomingForm();
-  form.parse(req, (err, fields, files) => {
+  try {
+    const file = resolve.apply(null, [__dirname].concat(req.url.split("/")))
+    var bodyStr = Object.keys(req.body).reduce((acc, key) => {
+      return acc + `${acc.length ? "&" : ""}${key}=${req.body[key]}`
+    }, "")
+    const cmd = `carton exec ${file} "${bodyStr}"`;
+    console.log(cmd);
+    exec(cmd, {cwd: file.substring(0, file.lastIndexOf("/")), maxBuffer: Infinity}, (err, stdout, stderr) => {
+      try {
+        if (err) {
+          console.error(err)
+          sendErr(err.message)
+        } else if (stderr) {
+          console.error(stderr)
+          sendErr(stderr)
+        } else if (stdout) {
 
-    try {
-      if (err) {
-        console.error(err)
-        return sendErr()
-      }
-      
-      const file = resolve.apply(null, [__dirname].concat(req.url.split("/")))
-      var bodyStr = Object.keys(fields).reduce((acc, key) => {
-        return acc + `${acc.length ? "&" : ""}${key}=${fields[key]}`
-      }, "")
-      exec(`carton exec ${file} "${bodyStr}"`, {cwd: file.substring(0, file.lastIndexOf("/")), maxBuffer: Infinity}, (err, stdout, stderr) => {
-        try {
-          if (err) {
-            console.error(err)
-            sendErr(err.message)
-          } else if (stderr) {
-            console.error(stderr)
-            sendErr(stderr)
-          } else if (stdout) {
-            const payload = stdout.replace(/^[^\{]*/g, "")
-            payload
-              ? res.json(JSON.parse(payload))
-              : res.status(200).send("OK")
-          } else {
-            res.status(400).send("Request Failed")
-          }
-        } catch (err) {
-          sendErr("Request Failed")
+          // TODO: implement more intelligent CGI response parsing.
+          const payload = stdout.replace(/^[^\{]*/g, "")
+          payload
+            ? res.json(JSON.parse(payload))
+            : res.status(200).send("OK")
+        } else {
+          res.status(400).send("Request Failed")
         }
-      })
-    } catch (err) {
-      console.error(err)
-      sendErr("Request Failed")
-    }
-  })
+      } catch (err) {
+        sendErr("Request Failed")
+      }
+    })
+  } catch (err) {
+    console.error(err)
+    sendErr("Request Failed")
+  }
 }
 
 app.get("/", (req, res) => {
   res.sendFile(resolve(__dirname, "index.html"))
 })
+
+app.use(bodyParser.json())
+app.post(/\.pl/, perl)
 
 app.use(require("webpack-hot-middleware")(compiler));
 const serveWebpack = middleware(compiler, {
@@ -68,7 +66,5 @@ app.get(/.*/, (req, res, next) => {
     serveWebpack(req, res, next)
   }
 })
-
-app.post(/\.pl/, perl)
 
 app.listen(process.env.PORT || 9090, () => console.log(`SQE server listening on port ${process.env.PORT || 9090}!`))
