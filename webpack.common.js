58084cf82033c1984982bd82e1f772a5254831df
sjones6 <jones.spencera@gmail.com>
2018-03-24 20:05:44 -0400

    correct public path for use in development server

--- webpack.common.js
+++ webpack.common.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.[name].js",
-    publicPath: "/dist/",
+    publicPath: "dist/",
   },
 
   module: {
