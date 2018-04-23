9add20d46be615fbf54d21d92b794f83c127c9f6
sjones6 <jones.spencera@gmail.com>
2018-04-23 07:23:49 -0400

    enable testing for server code

---
+++ tests/server/login-test.js
@@ -0,0 +1,29 @@
+const app = require('../../tools/app.js')
+const request = require('supertest')
+const assert = require('assert')
+
+describe('login', () => {
+
+  it('should attempt to login the test/asdf user', done => {
+    request(app)
+      .post('/resources/cgi-bin/scrollery-cgi.pl')
+      .send({
+        PASSWORD: "asdf",
+        SCROLLVERSION: 1,
+        USER_NAME: "test",
+        transaction: "validateSession"
+      })
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        assert(res.body.SESSION_ID.length > 5);
+        console.log(res);
+        done();
+      });
+  })
+
+})
