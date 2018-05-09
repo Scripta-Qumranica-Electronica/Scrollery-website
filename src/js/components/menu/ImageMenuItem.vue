21f2e9f98582e6858f228bab34e2143b76900cbf
Bronson Brown-deVost <bronsonbdevost@aim.com>
2018-05-09 11:58:01 +0200

    Fixed cursor visible in SingleImage pane.

--- src/js/components/menu/ImageMenuItem.vue
+++ src/js/components/menu/ImageMenuItem.vue
@@ -72,7 +72,7 @@ export default {
           params: {
             scrollID: this.scrollID,
             scrollVersionID: this.scrollVersionID,
-            colID: params.colID,
+            colID: this.$route.params.colID,
             imageID: this.imageID,
             artID: '~',
           },
