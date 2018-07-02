fa7a1fc1db91e5541c10473ba2d0ec9f343b0467
Bronson Brown-deVost <bronsonbdevost@aim.com>
2018-07-02 20:33:46 +0200

    Add mock localstorage to unit-tests.

--- tests/.utils/mount.js
+++ tests/.utils/mount.js
@@ -13,14 +13,43 @@ export default (Component, opt = {}) => {
   // i18n
   localVue.use(i18n, { store })
 
+  const window = {localStorage: storageMock()}
+
   // create args
   const args = {
     ...{
       store,
       localVue,
+      window,
     },
     ...opt,
   }
 
   return shallow(Component, args)
+}
+
+/**
+ * This is based on https://stackoverflow.com/questions/11485420/how-to-mock-localstorage-in-javascript-unit-tests
+ */
+function storageMock() {
+  var storage = {}
+
+  return {
+    setItem: function(key, value) {
+      storage[key] = value || ''
+    },
+    getItem: function(key) {
+      return key in storage ? storage[key] : null
+    },
+    removeItem: function(key) {
+      delete storage[key]
+    },
+    get length() {
+      return Object.keys(storage).length
+    },
+    key: function(i) {
+      var keys = Object.keys(storage)
+      return keys[i] || null
+    }
+  };
 }
