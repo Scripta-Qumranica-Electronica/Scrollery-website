12f4faf8a603515c409a87d23d042f993bba20d6
Itay Zandbank <itay@chelem.co.il>
2018-07-25 16:08:02 +0300

    Add basic Hebrew translations

--- src/js/lang/hb.js
+++ src/js/lang/hb.js
@@ -1,3 +1,26 @@
 module.exports = {
-  
+  // Globals
+  Loading: 'טוען',
+  Combinations: 'צירופים',
+  Artifact: 'ממצא',
+  Fragment: 'קטע',
+  'Global.Default': 'ברירית מחדל',
+
+  // Auth
+  Logout: 'יציאה',
+  'User.LoggedInMessage': 'שלום :name:',
+
+  // Errors
+  'Errors.Login': 'לא ניתן להכנס למערכת, הערכים באחד השדות שגויים',
+  'Errors.Unknown': 'התקבלה תשובה לא צפויה.',
+  'Errors.ServiceUnavailable': 'אי אפשר ליצור קשר עם השרת. נא לנסות במועד מאוחר יותר.',
+
+  // Menu
+  'Menu.Bars.Tooltip': 'לחצו לפתיחת התפריט',
+  'New Artefact': 'ממצא חדש',
+
+  // Editor
+  'Editor.HandOf': 'היד של',
+  'Editor.Fullscreen': 'מסך מלא',
+  'Editor.ToggleReconstructedText': 'הפעלת טקסט משוחזר',
 }
