a4b5fa210589784027475ab84303e672de481b3d
Bronson Brown-deVost <bronsonbdevost@aim.com>
2018-07-25 19:30:53 +0200

    Added German, dependency update.

--- src/js/lang/de.js
+++ src/js/lang/de.js
@@ -1,10 +1,31 @@
 module.exports = {
-    "Loading": "Laden",
-    "Combinations": "Kombinationen",
-    "Artifact": "Artefakt",
-    "Fragment": "Fragment",
-    "User.LoggedInMessage": "Angemeldet als: :name:",
-    "Logout": "Abmelden",
-    "Image": "Bild",
-    "Text": "Text"
+  // Globals
+  Loading: 'Laden',
+  Combinations: 'Kombinationen',
+  Artifact: 'Artefakt',
+  Fragment: 'Fragment',
+  'Global.Default': 'ברירית מחדל',
+
+  // Auth
+  Logout: 'Abmelden',
+  'User.LoggedInMessage': 'Angemeldet als: :name:',
+
+  // Errors
+  'Errors.Login': 'Bitte Fehler korrigieren und erneut einreichen',
+  'Errors.Unknown': 'Antwort unerwartet',
+  'Errors.ServiceUnavailable': 'Keine verbingung zum Server. Bitte versuchen Sie es noch einmal.',
+
+  // Menu
+  'Menu.Bars.Tooltip': 'Klicken Sie, um Menü offen zu halten',
+  'New Artefact': 'Neues Artefakt',
+
+  // Editor
+  'Editor.HandOf': 'Schrift von',
+  'Editor.Fullscreen': 'Vollbild',
+  'Editor.ToggleReconstructedText': 'rekonstruierter Text umschalten',
+
+  // Header
+  'No Scroll Selected': 'Keine Schriftrolle ausgewählt',
+  'Select Images': 'Bilder auswählen',
+  Zoom: 'Zoom',
 }
