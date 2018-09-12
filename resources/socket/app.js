191ebb9f723aea283de597b7b7d9b3801dbf2672
bronsonbdevost <bronsobdevost@aim.com>
2018-09-12 15:20:27 +0000

    Websockets with better loading.

--- resources/socket/app.js
+++ resources/socket/app.js
@@ -1,20 +1,29 @@
 const app = require('express')()
 const http = require('http').Server(app)
+const chokidar = require('chokidar')
 const io = require('socket.io')(http, {destroyUpgrade: false})
 
 const PORT = 6333
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html')
-})
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html')
+// })
 
-io.on('connection', function(socket){
-  console.log('a user connected')
-  socket.on('message', function(msg){
-    console.log(msg)
-    io.emit('message', msg);
-  });
-})
+// io.on('connection', function(socket){
+//   console.log('a user connected')
+//   socket.on('message', function(msg){
+//     console.log(`message: ${msg}`)
+//     io.emit('message', `Repeated: ${msg}`);
+//   });
+// })
+
+
+const loadSocket = (io) => {
+  const sockets = require('./sockets.js')
+  sockets.sockets(io)
+}
+
+loadSocket(io)
 
 http.listen(PORT, function(){
   console.log(`listening on *:${PORT}`)
